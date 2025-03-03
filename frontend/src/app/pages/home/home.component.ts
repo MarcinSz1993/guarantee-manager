0a8e78df679ceb98a915918d120502e76ebdfcf6
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-03-03 18:04:59 +0100

    Creating welcome page of the frontend layer.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
 }
