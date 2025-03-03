47b35ea9c2e9cbc07cc6777e3af93a7cf156c0b1
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-03-03 22:10:04 +0100

    Creating login page of the frontend layer.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NgForOf
+
   ],
   templateUrl: './home.component.html',
   standalone: true,
