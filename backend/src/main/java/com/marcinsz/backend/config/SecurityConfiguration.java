05cfff476b5014bd9f163b0284438f0c54129048
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-04-22 18:58:01 +0200

    Modifications

--- backend/src/main/java/com/marcinsz/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/marcinsz/backend/config/SecurityConfiguration.java
@@ -55,7 +55,7 @@ public class SecurityConfiguration {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("http://frontend"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
