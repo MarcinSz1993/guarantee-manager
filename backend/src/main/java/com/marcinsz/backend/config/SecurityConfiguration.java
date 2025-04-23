bb7ee4da1460d6a7471798857665b46612899e85
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-04-23 18:27:31 +0200

    Url domain configuration.

--- backend/src/main/java/com/marcinsz/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/marcinsz/backend/config/SecurityConfiguration.java
@@ -55,7 +55,7 @@ public class SecurityConfiguration {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://157.180.16.111:4200"));
+        config.setAllowedOrigins(Collections.singletonList("http://guarantee-manager.duckdns.org"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
