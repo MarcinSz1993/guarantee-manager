0acfb985760362c0a7e3df00c92106f7490ea4bd
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-04-29 20:48:20 +0200

    auth-guard corrections

--- frontend/src/app/own_services/token.service.ts
+++ frontend/src/app/own_services/token.service.ts
@@ -14,24 +14,24 @@ export class TokenService {
   ) { }
 
   setToken(token:string){
-    localStorage.setItem(this.tokenKey,token);
+    sessionStorage.setItem(this.tokenKey,token);
   }
 
   isUserLoggedIn(): boolean {
-    return localStorage.getItem(this.tokenKey) !== null;
+    return sessionStorage.getItem(this.tokenKey) !== null;
   }
 
   getToken():string | null {
-    return localStorage.getItem(this.tokenKey) as string;
+    return sessionStorage.getItem(this.tokenKey) as string;
   }
 
   getUsername():string | null {
-    return localStorage.getItem(this.username);
+    return sessionStorage.getItem(this.username);
   }
 
   logout(): void {
-    localStorage.removeItem(this.tokenKey);
-    localStorage.removeItem(this.username);
+    sessionStorage.removeItem(this.tokenKey);
+    sessionStorage.removeItem(this.username);
     sessionStorage.removeItem('firstname')
     sessionStorage.removeItem('lastname')
     sessionStorage.removeItem('notificationPreference')
@@ -42,17 +42,20 @@ export class TokenService {
     return !this.isTokenValid();
   }
 
-  private isTokenValid() {
-    const token = this.getToken()
-    if (!this.getToken()){
+  private isTokenValid(): boolean {
+    const token = this.getToken();
+    if (!token) {
       return false;
     }
+
     const jwtHelper = new JwtHelperService();
-    const isTokenExpired = jwtHelper.isTokenExpired(this.getToken());
-    if (isTokenExpired){
-      localStorage.clear();
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+
+    if (isTokenExpired) {
+      this.logout();
       return false;
     }
+
     return true;
   }
 }
