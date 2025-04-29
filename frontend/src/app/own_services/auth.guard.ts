5aba116eaadc084c2c0de2e497bfac8a5f088e80
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-04-29 20:42:45 +0200

    auth-guard corrections

--- frontend/src/app/own_services/auth.guard.ts
+++ frontend/src/app/own_services/auth.guard.ts
@@ -6,9 +6,13 @@ import {TokenService} from './token.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const tokenService = inject(TokenService);
   const router = inject(Router);
-  if (tokenService.isTokenNotValid() || !tokenService.isUserLoggedIn()){
-    router.navigate(['login']).then();
+
+  const isLoggedIn = !tokenService.isTokenNotValid() && tokenService.isUserLoggedIn();
+
+  if (!isLoggedIn) {
+    router.navigate(['/login']);
     return false;
   }
+
   return true;
 };
