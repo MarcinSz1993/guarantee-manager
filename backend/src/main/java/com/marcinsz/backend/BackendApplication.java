3ab34e0c3e997d206d91f5a1d693c3eec826449a
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-07-05 09:38:21 +0200

    Secure endpoint /api/pdf

--- backend/src/main/java/com/marcinsz/backend/BackendApplication.java
+++ backend/src/main/java/com/marcinsz/backend/BackendApplication.java
@@ -1,20 +1,16 @@
 package com.marcinsz.backend;
 
-import com.marcinsz.backend.pdf.PdfTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.io.IOException;
 
 @EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
     }
 }
