ffaafe5852d046423001e0a7335ace5696d7c016
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-07-14 22:17:57 +0200

    deploying new feature2

--- backend/src/main/java/com/marcinsz/backend/notification/dashboard/DashboardNotificationController.java
+++ backend/src/main/java/com/marcinsz/backend/notification/dashboard/DashboardNotificationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/dashboard")
+@RequestMapping("/api/dashboard")
 @RequiredArgsConstructor
 public class DashboardNotificationController {
     private final DashboardNotificationService dashboardNotificationService;
--- frontend/src/app/services/api.module.ts
+++ frontend/src/app/services/api.module.ts
@@ -14,6 +14,7 @@ import { ImageControllerService } from './services/image-controller.service';
 import { GuaranteeHistoryControllerService } from './services/guarantee-history-controller.service';
 import { EmailControllerService } from './services/email-controller.service';
 import { DashboardNotificationControllerService } from './services/dashboard-notification-controller.service';
+import { PdfControllerService } from './services/pdf-controller.service';
 import { AuditControllerService } from './services/audit-controller.service';
 import { LogoControllerService } from './services/logo-controller.service';
 import { DemoControllerService } from './services/demo-controller.service';
@@ -34,6 +35,7 @@ import { DemoControllerService } from './services/demo-controller.service';
     GuaranteeHistoryControllerService,
     EmailControllerService,
     DashboardNotificationControllerService,
+    PdfControllerService,
     AuditControllerService,
     LogoControllerService,
     DemoControllerService,
